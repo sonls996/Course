82bd14b5d7a6ff4188701b8d52093196f4ac103c
Lê Hồng Sơn <bapcaixanh009@gmail.com>
2020-03-31 21:09:36 +0700

    first commit

---
+++ js/education.js
@@ -0,0 +1,56 @@
+var btn = document.getElementById("toggleSearch").addEventListener("click",toggleLoggin);
+function toggleLoggin(){
+    var sot= document.getElementById("search");
+    sot.classList.toggle("ann");
+    // alert(" Thêm class thành công");
+}
+
+
+var btn = document.getElementById("toggleSearch1").addEventListener("click",toggleLoggin1);
+function toggleLoggin1(){
+    var sot= document.getElementById("search1");
+    sot.classList.toggle("ann");
+    // alert(" Thêm class thành công");
+}
+
+
+var hidden= document.getElementById("clickme");
+hidden.addEventListener("click",hiddenLogin);
+function hiddenLogin(){
+  if( document.getElementById("mobile-wrapLogin").style.height<="0px"){
+  document.getElementById("mobile-wrapLogin").style.height= "120px"
+  }else{
+    document.getElementById("mobile-wrapLogin").style.height="0px"
+  }
+}
+
+
+function openNav() {
+  document.getElementById("mySidebar").style.right = "0";
+  // document.getElementById("main").style.marginLeft = "250px";
+  document.getElementById("mySidebar").style.opacity = "1";
+}
+function closeNav() {
+  document.getElementById("mySidebar").style.right = "-100%";
+  // document.getElementById("main").style.marginLeft = "250px";
+  document.getElementById("mySidebar").style.opacity = "0";
+}
+
+
+
+
+    var acc = document.getElementsByClassName("accordion");
+    var i;
+    
+    for (i = 0; i < acc.length; i++) {
+      acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+          panel.style.maxHeight = null;
+        } else {
+          panel.style.maxHeight = panel.scrollHeight + "px";
+        } 
+      });
+    }
+   
